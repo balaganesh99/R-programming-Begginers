b39e31d45ab5ea750c7af3325a75b9913d482593
bala <balaganesh.balaganesh@gmail.com>
2017-06-09 17:36:08 -0400

    variables_push

---
+++ Types of variables/Types_of_variables.R
@@ -0,0 +1,32 @@
+
+#integer
+#All variable in R by defualt wil come as double. So we give 'L' with every integer.
+
+x <- 2L
+typeof(x)
+ 
+
+#double 
+
+y <- 2.5
+typeof(y)
+
+
+#complex
+
+z <- 3 + 2i
+typeof(z)
+
+
+#character
+
+a <- "this is a character"
+typeof(a)
+ 
+#logical 
+
+q1 <- T
+typeof(q1)
+
+q2 <- T 
+typeof(q2)
